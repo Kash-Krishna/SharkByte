28c0cc307b0cd560b1f319baf96e0dcd789a5f1f
Kash-Krishna <Kashys5@gmail.com>
2016-02-07 12:06:49 -0800

    Updated Grid Fields

--- deluge/ui/web/js/deluge-all/SearchGrid.js
+++ deluge/ui/web/js/deluge-all/SearchGrid.js
@@ -4,16 +4,36 @@ Deluge.SearchGrid = Ext.extend(Ext.grid.GridPanel, {
     title: 'Search Results',
     columns: [
         {
+            //id:'name'
             header: _('Name'),
+            width: 350,
             dataIndex: 'name'
         },{
             header: _('Size'),
+            width: 75,
             dataIndex: 'size'
+        },{
+            header: _('Date Uploaded'),
+            width: 90,
+            dataIndex: 'date'
+        },{
+            header: _('Uploader'),
+            width: 100,
+            dataIndex: 'uploader'
+        },{
+            header: _('Seeds'),
+            width: 60,
+            dataIndex: 'seeds'
+        },{
+            header: _('Leechers'),
+            width: 60,
+            dataIndex: 'leechers'
         }
     ],
 
-    height: 300,
-    width: 500
+    height: 100,
+    width: 200,
+   
     
 });
 
