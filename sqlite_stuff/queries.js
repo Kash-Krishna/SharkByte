ab3c4382781202eba9b236d7debcdcc4f280489e
Rachel Law <rlaw1294@gmail.com>
2016-02-08 02:20:57 -0800

    ready for demo

--- sqlite_stuff/queries.js
+++
@@ -1,8 +0,0 @@
-SELECT column1, column2, columnN FROM table_name
-
-SELECT $columns FROM $table WHERE $whereclause
-
-$columns = "magnet_link, torrent_name, seeders";
-$table = "local_youtor";
-$whereclause = ";";
-
