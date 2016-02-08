73435ad360f298d8506816638f8c087d65d6cee3
waoimapanda <khsie003@ucr.edu>
2016-02-07 18:55:07 -0800

    basic scraping works on extratorrent

--- web_crawler/bSoup.py
+++ web_crawler/bSoup.py
@@ -2,6 +2,6 @@ from bs4 import BeautifulSoup
 
 soup = BeautifulSoup(open("index.html"))
 
-#print(soup.prettify())
+print(soup.prettify())
 
-#print soup.title.string
+print soup.title.string
--- web_crawler/crawler.py
+++ web_crawler/crawler.py
@@ -1,7 +1,25 @@
 from bs4 import BeautifulSoup
-import request
+import urllib2
 
-r = request.get('https://reddit.com')
-soup = BeautifulSoup(r.text)
+if __name__ == "__main__":
+    
+    #x = 'https://reddit.com' # tested with reddit it works!! 
+    x = 'http://extratorrent.cc/'
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
 
-print(soup.prettify())
+    req = urllib2.Request(x, headers=hdr)
+    r = urllib2.urlopen(req).read()
+    soup = BeautifulSoup(r)
+    #print(soup.prettify())
+    links = []
+    for anchor in soup.find_all('a'):
+        links.append(anchor.get('href','/'))
+        #print(anchor.get('magnet:?')
+    for l in links:
+        if (l.find('http')):
+            print l
