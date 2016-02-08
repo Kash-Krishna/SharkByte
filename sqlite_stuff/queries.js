8a324b51d9f3df8960250697e9bc8de1253c2637
Rachel Law <rlaw1294@gmail.com>
2016-02-08 02:01:49 -0800

    sql stuff in progress

---
+++ sqlite_stuff/queries.js
@@ -0,0 +1,8 @@
+SELECT column1, column2, columnN FROM table_name
+
+SELECT $columns FROM $table WHERE $whereclause
+
+$columns = "magnet_link, torrent_name, seeders";
+$table = "local_youtor";
+$whereclause = ";";
+
