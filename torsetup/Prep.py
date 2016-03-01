eea78c5a1cb661fcfc723e3d89f382a1e3f47c0d
Kash-Krishna <Kashys5@gmail.com>
2016-03-01 05:53:26 -0800

    removing accidentally added files (still learning)

--- torsetup/Prep.py
+++
@@ -1,5 +0,0 @@
-import os
-
-with open("/etc/resolv.conf", "a") as f: f.write("nameserver 127.0.0.1\n")
-os.system("./torifyIPTables.sh")
-
--- torsetup/restoreIPTables.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-iptables -F
-iptables -t nat -F
